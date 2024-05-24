28f0c7fb1d66a95acc8087cb0493e7e5837e9d58
ark948 <armankarimi948@gmail.com>
2024-05-24 22:03:31 +0330

    Added permissions, auth, and login

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    # one new field
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    # CharField(read_only=True) could also be used
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
         # model serializer already has create and update methods
